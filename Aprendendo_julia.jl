2f6b67ef0ed591ea1b94ea3500942af013c33b6e
Matheus Carrijo de Brito <110065290+brito-matheus@users.noreply.github.com>
2023-03-24 09:17:47 -0300

    Update quarto notebook

--- Aprendendo_julia.jl
+++ Aprendendo_julia.jl
@@ -1,11 +1,33 @@
 using DataFrames
 using LinearAlgebra
 using Gadfly 
-using XLSX 
-
+using TableView
+using CSV
+using ExcelReaders
+using XLSX
 
 # Start Importing Fundos data set 
 
 fundos = XLSX.readxlsx("C:/Users/mathe/OneDrive/Documentos/Finanças/fundos.xlsx")
 
-fundos = DataFrame(fundos)
+dt = DataFrames.DataFrame(fundos["Sheet1"][:])
+
+column_names = Array{String}(dt[1, :])
+
+for i in eachindex(column_names)
+    column_names[i] = replace(column_names[i], "\n" => " ")
+    column_names[i] = replace(column_names[i], " " => "_")
+end
+
+# Rename the data frame 
+rename!(dt, column_names, makeunique=true)
+
+# Checking 
+
+names(dt)
+
+dt[:, [:Nome, :Classe, :Classificação_Anbima]]
+
+dropmissing(dt[!, [:Nome, :Classe, :Classificação_Anbima, :Gestora_]])
+
+
